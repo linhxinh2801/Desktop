786500c4b5c0ddbca574b7140d03573db6b26008
linhxinh2801 <2212404@dlu.edu.vn>
2024-11-28 11:06:06 +0700

    Add files via upload

---
+++ BT28_11/BT27.11/frmDanhSachDVKS.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace BT27._11
+{
+    public partial class frmDanhSachDVKS : Form
+    {
+        public frmDanhSachDVKS()
+        {
+            InitializeComponent();
+        }
+        private DataTable dichvuTable;
+        private void frmDanhSachDVKS_Load(object sender, EventArgs e)
+        {
+            this.LoadDichVu();
+        }
+        private void LoadDichVu()
+        { 
+            string connectionString = "server=PC328\\SQLEXPRESS; database = QuanLyDichVuKhachSan; Integrated Security = true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID_LoaiDV, TenLoai FROM LoaiDV";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            conn.Open();
+
+            adapter.Fill(dt);
+
+            conn.Close();
+            conn.Dispose();
+
+            cbbLoaiDV.DataSource = dt;
+
+            cbbLoaiDV.DisplayMember = "TenDV";
+
+            cbbLoaiDV.ValueMember = "ID_LoaiDV";
+
+        }
+
+        private void txtSearchByName_TextChanged(object sender, EventArgs e)
+        {
+            if (dichvuTable == null) return;
+            string filterExpression = "Name like '%" + txtSearchByName.Text + "%'";
+            string sortExpression = "Price DESC";
+            DataViewRowState rowStateFilter = DataViewRowState.OriginalRows;
+            DataView dataView = new DataView(dichvuTable, filterExpression, sortExpression, rowStateFilter);
+            dgvDanhSachDichVu.DataSource = dataView;
+        }
+
+        private void cbbLoaiDV_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cbbLoaiDV.SelectedIndex == -1) return;
+
+            string connectionString = "server=PC328\\SQLEXPRESS; database = QuanLyDichVuKhachSan; Integrated Security = true; ";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM LoaiDV WHERE ID_LoaiDV = @id";
+
+            // Truyền tham số
+            cmd.Parameters.Add("@id", SqlDbType.Int);
+
+            if (cbbLoaiDV.SelectedValue is DataRowView)
+            {
+                DataRowView rowView = cbbLoaiDV.SelectedValue as DataRowView;
+                cmd.Parameters["@id"].Value = rowView["ID_LoaiDV"];
+            }
+            else
+            {
+                cmd.Parameters["@id"].Value = cbbLoaiDV.SelectedValue;
+            }
+            // Tạo bộ điều phiếu dữ liệu
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            dichvuTable = new DataTable();
+
+            // Mở kết nối
+            conn.Open();
+
+            // Lấy dữ liệu từ csdl đưa vào DataTable
+            adapter.Fill(dichvuTable);
+
+            // Đóng kết nối và giải phóng bộ nhớ
+            conn.Close();
+            conn.Dispose();
+
+            // Đưa dữ liệu vào data gridview
+            dgvDanhSachDichVu.DataSource = dichvuTable;
+
+            // Tính số lượng mẫu tin
+            lblTongDichVu.Text = dichvuTable.Rows.Count.ToString();
+            lblLoaiDV.Text = cbbLoaiDV.Text;
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BT28_11/BT27.11/frmXoaCapNhatDV.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace BT27._11
+{
+    public partial class frmXoaCapNhatDV : Form
+    {
+        private void frmXoaCapNhatDV_Load(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        private void InitValues()
+        {
+            string connectionString = "server=.; database = RestaurantManagement; Integrated Security = true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT ID, Name FROM Category";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+
+            // mở kết nối
+            conn.Open();
+
+            // Lấy dữ liệu từ csdl đưa vào DataTable
+            adapter.Fill(ds, "Category");
+
+            // Hiển thị nhóm món ăn
+            cbbLoaiDV.DataSource = ds.Tables["Category"];
+            cbbLoaiDV.DisplayMember = "Name";
+            cbbLoaiDV.ValueMember = "ID";
+
+            // đóng kết nối và giải phóng bộ nhớ
+            conn.Close();
+            conn.Dispose();
+        }
+        private void ResetText()
+        {
+            txtMaDV.Text = "";
+            txtTenMatHang.ResetText();
+            cbbLoaiDV.ResetText();
+            txtDonGia.ResetText();
+            txtHinh.Text = "";
+            txtGhiChu.ResetText();
+
+        }
+
+        public void DisplayDichVuInfo(DataRowView rowView)
+        {
+            try
+            {
+                txtMaDV.Text = rowView["ID_LoaiDV"].ToString();
+                txtTenMatHang.Text = rowView["TenDV"].ToString();
+
+                cbbLoaiDV.SelectedIndex = -1;
+
+                // chọn nhóm món ăn tương ứng
+                for (int index = 0; index < cbbLoaiDV.Items.Count; index++)
+                {
+                    DataRowView cat = cbbLoaiDV.Items[index] as DataRowView;
+                    if (cat["ID"].ToString() == rowView["DichVu"].ToString())
+                    {
+                        cbbLoaiDV.SelectedIndex = index;
+                        break;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+                this.Close();
+            }
+        }
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnCapnhat_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = "server=.; database = QuanLyDichVuKhachSan; Integrated Security = true;";
+                SqlConnection conn = new SqlConnection(connectionString);
+
+                SqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "EXECUTE UpdateDV @id, @ten";
+
+                cmd.Parameters.Add("@id", SqlDbType.NVarChar, 100);
+                cmd.Parameters.Add("@ten", SqlDbType.NVarChar, 1000);
+
+                cmd.Parameters["@id"].Direction = ParameterDirection.Output;
+
+                cmd.Parameters["@id"].Value = txtMaDV.Text;
+                cmd.Parameters["@ten"].Value = txtTenMatHang.Text;
+
+                //mở kết nối.
+                conn.Open();
+
+                int numRowAffected = cmd.ExecuteNonQuery();
+
+                // Thông báo kết quả
+                if (numRowAffected > 0)
+                {
+                    MessageBox.Show("Successfully updating food", "Message");
+
+                    this.ResetText();
+                }
+                else
+                {
+                    MessageBox.Show("Updating food failed");
+                }
+
+                // đóng kết nối
+                conn.Close();
+                conn.Dispose();
+            }
+            catch (SqlException exception)
+            {
+                MessageBox.Show(exception.Message, "SQL Error");
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error");
+            }
+        }
+    }
+}
+
+
